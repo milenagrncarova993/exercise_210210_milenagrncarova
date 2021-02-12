64711ae828a9cff2f80922d1b4c29bcf1d2ae6b6
Milena Grncarova <milenagrncarov@outlook.com>
2021-02-12 09:20:35 +0100

    Finished all the crud methods

--- app/controllers/group_events_controller.rb
+++ app/controllers/group_events_controller.rb
@@ -1,11 +1,12 @@
 class GroupEventsController < ApplicationController
+  before_action :set_group_event, only: [:show, :edit, :update, :destroy]
 
   def index
       @group_events = GroupEvent.all
   end
 
   def show
-    @group_event = GroupEvent.find(params[:id])
+
   end
 
   def new
@@ -16,13 +17,34 @@ class GroupEventsController < ApplicationController
     @group_event = GroupEvent.new(group_event_params)
     @group_event.user = current_user
     @group_event.save
+
     redirect_to group_events_path(@group_event)
+  end
+
+  def edit
+
+  end
+
+  def update
+    @group_event.update(group_event_params)
+
+    redirect_to group_event_path(@group_event)
+  end
+
+  def destroy
+    @group_event.destroy
+
+    redirect_to group_event_path
   end
 
   private
 
   def group_event_params
     params.require(:group_event).permit(:name, :description, :location, :start_date, :end_date)
+  end
+
+  def set_group_event
+    @group_event = GroupEvent.find(params[:id])
   end
 
 
