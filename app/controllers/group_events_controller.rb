cf25904bb95108b57d38b0a1f2d16facff09a18e
Milena Grncarova <milenagrncarov@outlook.com>
2021-02-12 09:03:01 +0100

    Creating CRUD

--- app/controllers/group_events_controller.rb
+++ app/controllers/group_events_controller.rb
@@ -1,2 +1,29 @@
 class GroupEventsController < ApplicationController
+
+  def index
+      @group_events = GroupEvent.all
+  end
+
+  def show
+    @group_event = GroupEvent.find(params[:id])
+  end
+
+  def new
+    @group_event = GroupEvent.new 
+  end
+
+  def create
+    @group_event = GroupEvent.new(group_event_params)
+    @group_event.user = current_user
+    @group_event.save
+    redirect_to group_events_path(@group_event)
+  end
+
+  private
+
+  def group_event_params
+    params.require(:group_event).permit(:name, :description, :location, :start_date, :end_date)
+  end
+
+
 end
