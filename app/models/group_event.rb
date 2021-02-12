d93fa6c401db7ee707ea7110f629ed5454733de8
Milena Grncarova <milenagrncarov@outlook.com>
2021-02-12 13:48:55 +0100

    Created the show page of the events

--- app/models/group_event.rb
+++ app/models/group_event.rb
@@ -1,4 +1,11 @@
 class GroupEvent < ApplicationRecord
   belongs_to :user
   has_many :group_events
+  CATEGORY = [
+    "SAVED",
+    "DRAFTS"
+  ]
+  validates_presence_of :name, :location, :description, :start_date, :end_date
+  validates :status, inclusion: { in: CATEGORY }
+
 end
